c53d602befcf047472d4629bf7835eff97f9609b
tushar75037 <39720704+tushar75037@users.noreply.github.com>
2018-05-29 11:58:39 +0530

    menu added

--- project/main.cpp
+++ project/main.cpp
@@ -1,39 +1,15 @@
 #include <iostream>
-
+#include<iomanip>
 using namespace std;
 
-class Station
+//Required structures
+//for Time format
+struct Time
 {
-private:
-  char name[20];
-  char code[4];
-public:
-  void get();
-  void print_name();
-  void print_code();
+  int hour, minute, second;
 };
 
-class journey
-{
-private:
-  TIME time;
-  char source[20];
-  char destination[20];
-  Train train;
-  int customer;
-  float cost();
-  int facilities();
-  int distance;
-public:
-  void get();
-  void print_source();
-  void print_desination();
-  void print_train();
-  void print_cusromer();
-  void print_distance();
-};
-
-
+//Required classes
 //train class defines details about the respective trains
 class Train
 {
@@ -57,6 +33,47 @@ public:
   void show_speed(); //returns the speed of the train
 };
 
+class Station
+{
+private:
+  char name[20];
+  char code[4];
+public:
+  void get();
+  void print_name();
+  void print_code();
+};
+
+void Station::print_name()
+{
+  cout.write(name,20);
+}
+void Station::print_code()
+{
+  cout.write(code,4);
+}
+
+class Journey
+{
+private:
+  Time time;
+  char source[20];
+  char destination[20];
+  Train train;
+  int customer;
+  float cost();
+  int facilities();
+  int distance;
+public:
+  void get();
+  void print_source();
+  void print_desination();
+  void print_train();
+  void print_cusromer();
+  void print_distance();
+};
+
+
 //Customer class gives details about the customer
 class Customer
 {
@@ -70,9 +87,55 @@ public:
   void show_age();  //returns age of the customer
 };
 
+
+//other functions
+void print_ticket()//Train train , Journey journey,Customer customer,Station station)
+{
+  cout << "+----------------------------------------------------------------+" << endl;
+  cout << "|                           TICKET                               |" <<endl;
+  cout << "+----------------------------------------------------------------+" << endl;
+  cout << "|                                                 +------------+ |" << endl;
+  cout << "|                                                 | DATE       | |" << endl;
+  cout << "|                                                 +------------+ |" << endl;
+  cout << "|passenger name :Kanishk Debnath                                 |" << endl;
+  cout << "|passenger age  :20                                              |" << endl;
+  cout << "|from           :Del2 (DATE ; hr:min)                            |" << endl;
+  cout << "|to             :Del2 (DATE ; hr:min)                            |" << endl;
+  cout << "+----------------------------------------------------------------+" << endl;
+  cout << "|price          :rs 2000                                         |"<<endl;
+  cout << "|class          :A                                               |"<<endl;
+  cout << "|Train          :rajdhani express(RAJ21)                         |"<<endl;
+  cout << "|platform no    :xxx                                             |"<<endl;
+  cout << "|seat no        :x12                                             |"<<endl;
+  cout << "+----------------------------------------------------------------+" << endl;
+}
+
+void menu()
+{
+  cout<<"\t**************************************************\n";
+  cout<<"\t\tINDIAN RAILWAY REGISTRATION PORTAL\n";
+  cout<<"\t**************************************************\n";
+  cout<<"\t\t\t\tMENU\n";
+  cout<<"\t\t\t----------------\n";
+  cout<<"\t\t\t1.book ticket\n";
+  cout<<"\t\t\t2.check status\n";
+  cout<<"\t\t\t3.print ticket\n";
+  cout<<"\t\t\t4.station list\n";
+  cout<<"\t\t\t5.train list\n";
+  cout<<"\t\t\t6.EXIT\n\n\n";
+  cout<<"\t\t\tchoice: ";
+
+}
+
 int main()
 {
-
+  menu();
+  // for(int i = 0; i < 3; i++)
+  // {
+  //   cout << "person " << i + 1 << endl;
+  //   print_ticket();
+  //   cout << "\n\n" <<endl;
+  // }
   return 0;
 }
 void Station::get()
@@ -81,12 +144,4 @@ void Station::get()
   cin.getline(name,20);
   cout<<"please enter station code\n";
   cin.getline(code,4);
-}
-void station::print_name()
-{
-  cout.write(name,20);
-}
-void station::print_code()
-{
-  cout.write(code,4);
 }
